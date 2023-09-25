28fc6fdb0fb7d997fb26f2506a4998898031d2a5
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-09-25 02:59:12 +0100

    Add files via upload

---
+++ UF1845PPF/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Componentes
+import { HeaderComponent } from './header/header.component';
+import { ListarCursosComponent } from './componentes/listar-cursos/listar-cursos.component';
+import { CrearCursosComponent } from './componentes/crear-cursos/crear-cursos.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { CursoComponent } from './componentes/curso/curso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListarCursosComponent,
+    CrearCursosComponent,
+    HomeComponent,
+    ContactoComponent,
+    CursoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    AppRoutingModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
