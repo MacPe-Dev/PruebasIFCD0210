2fa00f3d1fcbe3dea756a13b8d5a9894eea476c4
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-09-25 03:57:51 +0100

    Add files via upload

--- UF1845PPF/nodeserve/config/db.js
+++ UF1845PPF/nodeserve/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'variables.env' });
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO3,{
+        await mongoose.connect(process.env.DB_MONGO,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
